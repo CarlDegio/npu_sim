2f20c5c9c81d48b73b4162ce3f968d81c6b3b7af
CarlDegio <2536890272Carl@gmail.com>
2022-01-26 23:35:14 +0800

    重大更新，使用xacro重新建模小车与机械臂，基本完成

--- README.md
+++ README.md
@@ -9,10 +9,6 @@ sudo apt install ros-foxy-xacro
 sudo apt install ros-foxy-gazebo-ros2-control
 ```
 
-
-
-待补充
-
 ## 文件系统
 
 config：
@@ -25,41 +21,23 @@ launch：
 
 存放启动文件，遵循ros2约定的py形式，主要在使用的是carlaunch_gazebo.py。
 
-carlaunch.py是一个rviz内的模型预览，
-
-carlaunch_xacro.py是转化xacro为urdf并在rivz内预览。
-
 
 
 urdf：
 
-模型文件，现在主要使用的是car_gazebo.urdf，有时间再改成xacro，使得路径自动化find。
+car.xacro是车外形的描述文件，在launch中再被转化为urdf。
 
-mobot.example是借鉴其他人的一个四轮小车模型，链接[古月居](https://www.guyuehome.com/6788)，是一个雷达+相机的四轮小车。
-
-four_macnum也是借鉴他人的麦轮小车，[链接](https://github.com/MapleHan/four_macnum_car)
-
-car.urdf和car.xacro只有一个方块，供carlaunch.py和carlaunch_xacro.py检验用。
+car.gazebo.xacro为车增添了gazebo中的插件与颜色配置，后缀是.xacro只是为了能够在pycharm下语法标记。
 
 
 
 world：
 
-世界文件，现在主要使用的是default.world，其中包含了两根地图的柱子，以检验相机和雷达功能。
-
-car.world是一个将模型和外界都写在world内的尝试，但发现雷达有问题，雷达信息所属的frame无法获得。暂时搁置了。
+世界文件，使用的是default.world，其中包含了两根地图的柱子，以检验相机和雷达功能。
 
 ## 运行
 
-包clone在工作空间src下，修改car_gazebo.urdf文件内这部分内容：
-
-```xml
-<parameters>/home/demons/dev_ws/install/npu_sim/share/npu_sim/config/joint_controller.yaml</parameters>
-```
-
-这里demons改为自己的用户名（我的电脑用户名是demons），dev_ws改成自己的工作空间名（或者你的工作空间在其他路径，总之install之前的路径在不同环境下可能不一样）。这是一个将修正的问题，等我用xacro替代了这个urdf，它应当会被解决。
-
-然后编译，之后即可运行
+在工作空间下编译，之后即可运行
 
 ```
 ros2 launch npu_sim carlaunch_gazebo.py
@@ -67,25 +45,43 @@ ros2 launch npu_sim carlaunch_gazebo.py
 
 同时打开gazebo和rviz2，能看到雷达线即表明成功。
 
-还可使用rqt查看相机图像。
+
+
+传感：
+
+相机的传感主要有`/car/camera_high`和`/car/camera_low`两个空间之下的，前者是在机械臂上固定的深度相机，后者是车载的可以被驱动绕y旋转的RGB相机。可以通过rqt中的image看到不同的图片。
+
+雷达的传感是`/car/scan`，扫描角度为[-1.5,1.5]弧度，一循环采样500个样点，一秒采集10次，最大距离10m最小距离0.1m。在rviz中即可看到。
+
+imu的传感是`/car/imu`，给出各向线加速度，角速度，角，包含重力和噪声。
+
+
 
 控制：
 
-车底盘采用`libgazebo_ros_planar_move.so`，这是一个偷懒插件，以绝对性的移动替代了轮子，对应的控制话题是`/car/cmd_vel`，也可以通过rqt的topic publisher看到其中包含的数据类型等。以对应的数据类型发布速度即可使车运动或旋转。
+车底盘采用`libgazebo_ros_planar_move.so`，这是一个偷懒插件，以绝对性的移动替代了轮子，对应的控制话题是`/car/cmd_vel`，也可以通过rqt的topic publisher看到其中包含的数据类型等。以对应的数据类型发布速度即可使车运动或旋转。**存在的bug：planar和ros2_control貌似都跳过了动力学，直接设置速度而加速度置为0，从而复杂模型之间的交互力会错误。导致存在其中一个插件时，仿真的运动会变得错误（如期望自转速度1rad/s，实际无法到达），表现为整体仿真运动的更慢，需要更大的力才能正常运动**
 
-相机还带有一个旋转关节，控制话题为`/joint_camera_controller/commands`可以直接控制相机角度，它的话题构造目前我只探索出了命令发布，给出一个例子。
+机械臂带有三个旋转关节，车载相机带有一个旋转关节（代表现实的舵机）。机械臂的控制话题为`/arm_joint_controller/commands`可以控制机械臂每个关节的速度，ABB构型，底座是绕z旋转的，其余两个绕y旋转，x向前，旋转的正方向符合右手定则；车载相机控制话题为`/camera_joint_controller/commands`。这种格式的话题构造目前我只探索出了命令发布（代码也可以），给出一个例子。
 
 ```
 ros2 topic pub /joint_camera_controller/commands std_msgs/msg/Float64MultiArray "data:
-- 0.5"
+- 0.5
+- 0.1
+- -0.1"
 ```
 
 注意这里的冒号后有换行，大概是直接发布的double数组，但还不知道在rqt中如何构造这样的数据类型。
 
-
+可以通过`/dynamic_joint_states`和`/joint_states`两个话题接收四个关节数据，两个应该没有本质差别。
 
 ## 拓展
 
 gazebo11安装好后，`/opt/ros/foxy/share/gazebo_plugins/worlds`中有一些sdf案例，如各种车的仿真控制，传感器信息接收等，附带有可以尝试的指令，可以从这里了解到一些常用的组件的sdf代码。使用前最好预先[下载](https://github.com/osrf/gazebo_models)model到~/.gazebo/models
 
 `ros-foxy-gazebo-ros2-control-demos`这个包包含了ros2 control在gazebo中的使用案例，是位置、速度、力控制的cartpole，可以试试向他们发送话题等，[仓库链接](https://github.com/ros-simulation/gazebo_ros2_control)。ros2的controller和ros1不太一样，还在摸索中。另一个教学型[案例](https://github.com/ros-controls/ros2_control_demos)以及官方[doc](https://ros-controls.github.io/control.ros.org/index.html)，内容还很少。
+
+
+
+ros2 仿真建模实例：https://www.guyuehome.com/6788，mobot系列。
+
+four_macnum是他人的麦轮小车，[链接](https://github.com/MapleHan/four_macnum_car)
--- launch/carlaunch_gazebo.py
+++ launch/carlaunch_gazebo.py
@@ -1,32 +1,60 @@
 import os
+
+import xacro
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
 from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
-    urdf = os.path.join(get_package_share_directory('npu_sim'), 'urdf', 'car_gazebo.urdf')
-    assert os.path.exists(urdf)
+    world_dir = os.path.join(get_package_share_directory('npu_sim'), 'world', 'default.world')
+    assert os.path.exists(world_dir)  # no exist
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+        launch_arguments={'world': world_dir}.items(),
+    )
+
+    xacro_file = os.path.join(get_package_share_directory('npu_sim'), 'urdf', 'car.xacro')
+    assert os.path.exists(xacro_file)
+
+    doc=xacro.parse(open(xacro_file))
+    xacro.process_doc(doc)
+    robot_description_config=doc.toxml()
+    robot_description = {'robot_description': robot_description_config}
+
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[robot_description]
+    )
 
     rviz_config_dir = os.path.join(get_package_share_directory('npu_sim'), 'config', 'world.rviz')
     assert os.path.exists(rviz_config_dir)
 
-    world_dir = os.path.join(get_package_share_directory('npu_sim'), 'world', 'default.world')
-    assert os.path.exists(world_dir)  # no exist
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'arm_car'],
+                        output='screen')
 
-    xml = open(urdf, 'r').read()
-    xml = xml.replace('"', '\\"')
-    spwan_args = '{name: \"arm_car\", xml: \"' + xml + '\" }'
+    load_joint_state_broadcaster=ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start','joint_state_broadcaster'],
+        output='screen'
+    )
 
-    gazebo = ExecuteProcess(cmd=['gazebo', '--verbose', world_dir,
-                                 '-s', 'libgazebo_ros_factory.so'], output='screen')
+    load_joint_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start', 'arm_joint_controller'],
+        output='screen'
+    )
 
-    spawn_entity=ExecuteProcess(
-            cmd=['ros2', 'service', 'call', '/spawn_entity', 'gazebo_msgs/SpawnEntity', spwan_args],
-            output='screen')
+    load_camera_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start', 'camera_joint_controller'],
+        output='screen'
+    )
 
     rviz2=Node(
             package='rviz2',
@@ -35,60 +63,27 @@ def generate_launch_description():
             arguments=['-d', rviz_config_dir],
             output='screen')
 
-    node_robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='screen',
-        arguments=[urdf])
-
-    load_joint_state_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start',
-             'joint_camera_broadcaster'],
-        output='screen'
-    )
-
-    load_forward_command_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start',
-             'joint_camera_controller'],
-        output='screen'
-    )
-
     return LaunchDescription([
         RegisterEventHandler(
             event_handler=OnProcessExit(
                 target_action=spawn_entity,
-                on_exit=[load_joint_state_controller],
+                on_exit=[load_joint_state_broadcaster],
             )
         ),
         RegisterEventHandler(
             event_handler=OnProcessExit(
-                target_action=load_joint_state_controller,
-                on_exit=[load_forward_command_controller],
+                target_action=load_joint_state_broadcaster,
+                on_exit=[load_joint_controller],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=load_joint_controller,
+                on_exit=[load_camera_controller],
             )
         ),
         gazebo,
-        spawn_entity,
         rviz2,
         node_robot_state_publisher,
-        # Node(
-        #     package='turtlesim',
-        #     namespace='turtlesim1',
-        #     executable='turtlesim_node',
-        #     name='sim'
-        # ),
-        # Node(
-        #     package='turtlesim',
-        #     namespace='turtlesim2',
-        #     executable='turtlesim_node',
-        #     name='sim'
-        # ),
-        # Node(
-        #     package='turtlesim',
-        #     executable='mimic',
-        #     name='mimic',
-        #     remappings=[
-        #         ('/input/pose', '/turtlesim1/turtle1/pose'),
-        #         ('/output/cmd_vel', '/turtlesim2/turtle1/cmd_vel'),
-        #     ]
-        # )
+        spawn_entity
     ])
